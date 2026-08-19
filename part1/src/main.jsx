ef486e72ecbf67f0ce821d312db51deae70312c0
Santiago Abad <santiagoabad@gmail.com>
2026-08-19 23:22:04 +0200

    feat(part1): update counter by the second with setInterval method

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,7 +1,15 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-let counter = 10;
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App counter={counter} />,
-);
+let counter = 1;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const refresh = () => {
+  root.render(<App counter={counter} />);
+};
+
+setInterval(() => {
+  refresh();
+  counter += 1;
+}, 1000);
