48bf29bb9c1d333c00990729eb74c458ecde872e
Santiago Abad <santiagoabad@gmail.com>
2026-08-19 15:35:02 +0200

    feat(part1): add multiple components

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,19 +1,19 @@
-const App = () => {
-  console.log("Hello from component");
-
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-
-  console.log(now, a + b);
-
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <p>Hello {props.name}</p>
     </div>
   );
 };
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="George" />
+      <Hello name="Daisy" />
+    </div>
+  );
+};
+
 export default App;
