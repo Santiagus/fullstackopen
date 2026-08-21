00565aeb6e6404170ffb2435eaf96eeaf96bd14e
Santiago Abad <santiagoabad@gmail.com>
2026-08-21 19:39:56 +0200

    refactor: more profesional code, no new features or bug fixes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,36 +1,40 @@
 import { useState } from "react";
 
+const anecdotes = [
+  "If it hurts, do it more often.",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+  "The only way to go fast, is to go well.",
+];
+
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * max);
 };
 
-const Annectode = ({ text, value }) => {
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-    "The only way to go fast, is to go well.",
-  ];
-  console.log("anecdotes.length:", anecdotes.length);
-  console.log("Random index:", getRandomInt(anecdotes.length));
-  const [selected, setSelected] = useState(getRandomInt(anecdotes.length));
-
+const Anecdote = ({ text }) => {
   return (
     <div>
       <h1>Anecdotes</h1>
-      {anecdotes[selected]}
+      <p>{text}</p>
     </div>
   );
 };
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <Annectode />;
-}
+const App = () => {
+  const [selected, setSelected] = useState(getRandomInt(anecdotes.length));
+  const nextAnecdote = () => {
+    setSelected(getRandomInt(anecdotes.length));
+  };
+  return (
+    <>
+      <Anecdote text={anecdotes[selected]} />
+      <button onClick={nextAnecdote}>Next anecdote</button>
+    </>
+  );
+};
 
 export default App;
