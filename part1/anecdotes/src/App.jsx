e85efe85219fb917b7d25d5b026d5994d6381434
Santiago Abad <santiagoabad@gmail.com>
2026-08-21 19:00:06 +0200

    feat(anecdotes): add anecdotes component to display random anecdotes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,15 +1,36 @@
 import { useState } from "react";
 
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+const Annectode = ({ text, value }) => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  console.log("anecdotes.length:", anecdotes.length);
+  console.log("Random index:", getRandomInt(anecdotes.length));
+  const [selected, setSelected] = useState(getRandomInt(anecdotes.length));
+
+  return (
+    <div>
+      <h1>Anecdotes</h1>
+      {anecdotes[selected]}
+    </div>
+  );
+};
+
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <h1>Get started</h1>
-      </div>
-    </>
-  );
+  return <Annectode />;
 }
 
 export default App;
