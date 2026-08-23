d9b17ee0a425111cdad92e1abd58e3f06956b8a9
Santiago Abad <santiagoabad@gmail.com>
2026-08-23 17:05:54 +0200

    feat: list all courses and their parts with the number of exercises in each part

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,9 +1,8 @@
 import Course from "./components/Course";
-
-const App = () => {
-  const course = {
-    id: 1,
+const courses = [
+  {
     name: "Half Stack application development",
+    id: 1,
     parts: [
       {
         name: "Fundamentals of React",
@@ -20,10 +19,33 @@ const App = () => {
         exercises: 14,
         id: 3,
       },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
     ],
-  };
+  },
+  {
+    name: "Node.js",
+    id: 2,
+    parts: [
+      {
+        name: "Routing",
+        exercises: 3,
+        id: 1,
+      },
+      {
+        name: "Middlewares",
+        exercises: 7,
+        id: 2,
+      },
+    ],
+  },
+];
 
-  return <Course course={course} />;
+const App = () => {
+  return <Course courses={courses} />;
 };
 
 export default App;
--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,17 +1,19 @@
-const Course = ({ course }) => {
-  return (
-    <div>
-      <h1>{course.name}</h1>
-      <ul>
-        {course.parts.map(part => (
-          <li key={part.id}>
-            {part.name}: {part.exercises} exercises
-          </li>
-        ))}
-      </ul>
-      <p>Total of {course.parts.reduce((sum, part) => sum + part.exercises, 0)} exercisesgs</p>
-    </div>
-  );
+const Course = ({ courses }) => {
+  return courses.map(course => (
+    <>
+      <div key={course.id}>
+        <h1>{course.name}</h1>
+        <ul>
+          {course.parts.map(part => (
+            <li key={part.id}>
+              {part.name}: {part.exercises} exercises
+            </li>
+          ))}
+        </ul>
+      </div>
+      <p>Total of {course.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</p>
+    </>
+  ));
 };
 
 export default Course;
