b7e935150f38f9569ae1a088551ad61b8b86c96c
Santiago Abad <santiagoabad@gmail.com>
2026-08-24 13:50:49 +0200

    feat(part2): phonebook names printing

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 
 function App() {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }, { name: "Phillip Philiphos" }]);
   const [newName, setNewName] = useState("");
+  const Names = ({ persons }) => {
+    const allNames = persons.map(person => <ul>{person.name}</ul>);
+    return <div>{allNames}</div>;
+  };
 
   return (
     <>
@@ -11,11 +15,15 @@ function App() {
         <div>
           name : <input />
         </div>
+        <Names persons={persons} />
       </form>
       <div>
         <button type="submit"> add </button>
       </div>
       <h2>Numbers</h2>
+
+      <h2>Debug:</h2>
+      <div>{newName}</div>
     </>
   );
 }
