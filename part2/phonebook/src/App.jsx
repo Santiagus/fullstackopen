7dca5f1bedbf0c0ae18b8a4c41ac5b0c4603fb82
Santiago Abad <santiagoabad@gmail.com>
2026-08-24 19:18:50 +0200

    refactor: split components into separate files

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,7 @@
 import { useState } from "react";
-
-const Names = ({ persons, filter }) => {
-  const searchTerm = filter.trim().toLowerCase();
-  const filteredPersons =
-    searchTerm === "" ? persons : persons.filter(person => person.name.toLowerCase().includes(searchTerm));
-
-  return (
-    <ul>
-      {filteredPersons.map(person => (
-        <li key={person.name}>
-          {person.name} - {person.number}
-        </li>
-      ))}
-    </ul>
-  );
-};
+import Persons from "./components/Persons";
+import PersonForm from "./components/PersonForm";
+import Filter from "./components/Filter";
 
 function App() {
   const [persons, setPersons] = useState([
@@ -52,28 +39,20 @@ function App() {
       return;
     }
 
-    setPersons(persons.concat({ name: newPerson.name, number: newPerson.number }));
+    setPersons(
+      persons.concat({ name: newPerson.name, number: newPerson.number, id: Math.max(...persons.map(p => p.id)) + 1 }),
+    );
     setNewPerson({ name: "sample name", number: "666448877" });
   };
 
   return (
     <>
       <h2>Phonebook</h2>
-      filter shown with : <input value={filter} onChange={handleFilterChange} />
+      <Filter value={filter} onChange={handleFilterChange} />
       <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          <p>
-            name : <input name="name" value={newPerson.name} onChange={handlePersonChange} />
-          </p>
-          <p>
-            number : <input name="number" value={newPerson.number} onChange={handlePersonChange} />
-          </p>
-        </div>
-        <button type="submit"> add </button>
-      </form>
+      <PersonForm onSubmit={addPerson} person={newPerson} onChange={handlePersonChange} />
       <h2>Numbers</h2>
-      <Names persons={persons} filter={filter} />
+      <Persons persons={persons} filter={filter} />
       <h2>Debug:</h2>
       <div>
         Name: {newPerson.name} - Number: {newPerson.number}
