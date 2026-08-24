056dd7d1158794ece497b70188f443b0d128c9e0
Santiago Abad <santiagoabad@gmail.com>
2026-08-24 16:41:56 +0200

    feat(part2): add names to the state and display them in the UI

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,47 @@
 import { useState } from "react";
 
+const Names = ({ persons }) => {
+  const allNames = persons.map(person => <ul key={person.name}>{person.name}</ul>);
+  return <div>{allNames}</div>;
+};
+
 function App() {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }, { name: "Phillip Philiphos" }]);
-  const [newName, setNewName] = useState("");
-  const Names = ({ persons }) => {
-    const allNames = persons.map(person => <ul>{person.name}</ul>);
-    return <div>{allNames}</div>;
+  const [newName, setNewName] = useState("sample name");
+
+  const handleNameChange = event => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const addName = event => {
+    event.preventDefault();
+
+    if (newName === "") {
+      alert(`No name entered`);
+      return;
+    }
+
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already in the phonebook`);
+      return;
+    }
+
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
   };
 
   return (
     <>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name : <input />
+          name : <input value={newName} onChange={handleNameChange} />
         </div>
-        <Names persons={persons} />
-      </form>
-      <div>
         <button type="submit"> add </button>
-      </div>
+      </form>
       <h2>Numbers</h2>
+      <Names persons={persons} />
 
       <h2>Debug:</h2>
       <div>{newName}</div>
