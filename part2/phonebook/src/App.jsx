3b892f923027a5c9dd136f9a715cc95860f061b9
Santiago Abad <santiagoabad@gmail.com>
2026-08-24 18:13:40 +0200

    chore: add more initial names to the phonebook app

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -18,8 +18,10 @@ const Names = ({ persons, filter }) => {
 
 function App() {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "644554466" },
-    { name: "Phillip Philiphos", number: "644554477" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newPerson, setNewPerson] = useState({ name: "sample name", number: "666448877" });
   const [filter, setFilter] = useState("");
