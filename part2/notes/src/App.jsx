af5b14d1f70d7b13c12d42bfa18fc8220e361698
Santiago Abad <santiagoabad@gmail.com>
2026-08-25 21:56:50 +0200

    refactor: update service to return only the used data

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -2,17 +2,14 @@ import Note from "./components/Note";
 import { useState, useEffect } from "react";
 import noteService from "./services/notes";
 
-
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    console.log("effect");
     noteService.getAll().then(initialNotes => {
-      console.log("Notes fetched from server:", initialNotes.data);
-      setNotes(initialNotes.data);
+      setNotes(initialNotes);
     });
   }, []);
 
@@ -29,9 +26,8 @@ const App = () => {
 
     noteService
       .create(noteObject)
-      .then(response => {
-        console.log("Note added to server:", response.data);
-        setNotes(notes.concat(response.data));
+      .then(returnedNotes => {
+        setNotes(notes.concat(returnedNotes));
         setNewNote("Note " + notes.length);
       })
       .catch(error => {
@@ -40,22 +36,19 @@ const App = () => {
   };
 
   const handleNoteChange = event => {
-    console.log(event.target.value);
     setNewNote(event.target.value);
   };
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important === true);
 
   const toggleImportanceOf = id => {
-    console.log(`Toggling importance of note with id: ${id}`);
     const note = notes.find(n => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
     noteService
       .update(id, changedNote)
-      .then(response => {
-        console.log("Note updated on server:", response.data);
-        setNotes(notes.map(n => (n.id !== id ? n : response.data)));
+      .then(returnedNotes => {
+        setNotes(notes.map(n => (n.id !== id ? n : returnedNotes)));
       })
       .catch(error => {
         console.error("Error updating note:", error);
