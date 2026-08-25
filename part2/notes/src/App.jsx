24a1cc25be535c22d6b0dfd22f790a98eac720f0
Santiago Abad <santiagoabad@gmail.com>
2026-08-25 21:45:04 +0200

    refactor: move service code to a separate file under src/services/notes.js

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,8 +1,7 @@
 import Note from "./components/Note";
 import { useState, useEffect } from "react";
-import axios from "axios";
+import noteService from "./services/notes";
 
-const SERVER_URL = "http://localhost:3001/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -11,11 +10,12 @@ const App = () => {
 
   useEffect(() => {
     console.log("effect");
-    axios.get(SERVER_URL).then(response => {
-      console.log("Notes fetched from server:", response.data);
-      setNotes(response.data);
+    noteService.getAll().then(initialNotes => {
+      console.log("Notes fetched from server:", initialNotes.data);
+      setNotes(initialNotes.data);
     });
   }, []);
+
   console.log("render", notes.length, "notes");
 
   const addNote = event => {
@@ -27,8 +27,8 @@ const App = () => {
     event.preventDefault();
     console.log("button clicked", event.target);
 
-    axios
-      .post(SERVER_URL, noteObject)
+    noteService
+      .create(noteObject)
       .then(response => {
         console.log("Note added to server:", response.data);
         setNotes(notes.concat(response.data));
@@ -51,8 +51,8 @@ const App = () => {
     const note = notes.find(n => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
-    axios
-      .put(`${SERVER_URL}/${id}`, changedNote)
+    noteService
+      .update(id, changedNote)
       .then(response => {
         console.log("Note updated on server:", response.data);
         setNotes(notes.map(n => (n.id !== id ? n : response.data)));
