7eb30032e5f2f1a15f3411923ab483eb563c412d
Santiago Abad <santiagoabad@gmail.com>
2026-08-25 17:26:28 +0200

    feat: phonebook app fetch persons from json-server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
+import axios from "axios";
+
+const SERVER_URL = "http://localhost:3001/persons";
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([,]);
   const [newPerson, setNewPerson] = useState({ name: "sample name", number: "666448877" });
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get(SERVER_URL).then(response => {
+      console.log("Persons fetched from server:", response.data);
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "persons");
 
   const handlePersonChange = event => {
     const { name, value } = event.target;
