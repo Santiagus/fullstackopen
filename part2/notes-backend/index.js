e25598e048843b2b3ea772c58c1d3ff94bbefc86
Santiago Abad <santiagoabad@gmail.com>
2026-08-28 13:29:41 +0200

    feat: A new feature for the user or a significant enhancement
    build: server restart automatically on code updates

--- part2/notes-backend/index.js
+++ part2/notes-backend/index.js
@@ -25,7 +25,7 @@ let notes = [
 // });
 
 app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
+  response.send("<h1>Hello World !</h1>");
 });
 
 app.get("/api/notes", (request, response) => {
