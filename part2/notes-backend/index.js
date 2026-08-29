7c6e26a551f6ec01d137016f75de53ed5c23c826
Santiago Abad <santiagoabad@gmail.com>
2026-08-29 18:05:52 +0200

    chore: console.log for deleted note

--- part2/notes-backend/index.js
+++ part2/notes-backend/index.js
@@ -48,7 +48,7 @@ app.get("/api/notes/:id", (request, response) => {
 app.delete("/api/notes/:id", (request, response) => {
   const id = request.params.id;
   notes = notes.filter(note => note.id !== id);
-
+  console.log(`Note ${id} deleted`);
   response.status(204).end();
 });
 
