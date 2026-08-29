42c35033659a6f68c2bc92404ad9dc5bbf4ebee3
Santiago Abad <santiagoabad@gmail.com>
2026-08-29 20:08:47 +0200

    feat: error handling for note content missing

--- part2/notes-backend/index.js
+++ part2/notes-backend/index.js
@@ -56,12 +56,28 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
-app.post("/api/notes", (request, response) => {
+const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0;
-  const note = request.body;
-  note.id = String(maxId + 1);
+  return String(maxId + 1);
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false, // default to false
+    id: generateId(),
+  };
+
   notes = notes.concat(note);
-  console.log(`Created : `, note);
+
   response.json(note);
 });
 
