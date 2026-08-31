94493f768191cb2eb57c59afc7892443d944900b
Santiago Abad <santiagoabad@gmail.com>
2026-08-31 23:58:31 +0200

    feat: add mongoose validation schema

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -39,13 +39,6 @@ app.get("/api/notes/:id", async (request, response) => {
 
 app.post("/api/notes", async (request, response) => {
   const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
@@ -102,6 +95,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
