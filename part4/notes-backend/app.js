18df818702ef154e75cfc902d455d576f65b6493
Santiago Abad <santiagoabad@gmail.com>
2026-09-01 17:23:22 +0200

    feat: static FE intead of Hello Word at /

--- part4/notes-backend/app.js
+++ part4/notes-backend/app.js
@@ -1,31 +1,13 @@
 const express = require('express')
+const middleware = require('./utils/middleware')
 const notesRouter = require('./controllers/notes')
 
 const app = express()
 
+app.use(express.static('dist'))
 app.use('/api/notes', notesRouter)
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-app.use(errorHandler)
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(unknownEndpoint)
+app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
 
 module.exports = app
